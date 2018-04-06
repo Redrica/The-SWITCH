d71c6df7ec4b1b11f7ca4162b7d9c1ce3f3ee7c8
Redrica <de.spring@gmail.com>
2018-04-06 19:10:08 +0300

    Add gulp tasks for docs folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
-// "use strict";
+"use strict";
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync');
+var del = require('del');
+var run = require('run-sequence');
 
 gulp.task('style', function () {
   return gulp.src('source/sass/style.scss')
@@ -26,26 +28,76 @@ gulp.task('watch', ['browser-sync', 'style'], function () {
   gulp.watch('source/js/*.js', browserSync.reload);
 });
 
+gulp.task('copy', function () {
+  return gulp.src([
+    'source/css/*.css',
+    'source/fonts/*.{woff,woff2}',
+    'source/img/*.{png,jpg}',
+    'source/js/*.js',
+    'source/*.html'
+  ], {
+    base: 'source'
+  })
+    .pipe(gulp.dest('docs'));
+});
+
+gulp.task('clean', function () {
+  return del(['docs/**', 'docs/*.*', '!docs', '!docs/Readme.md']);
+});
+
+gulp.task('build', function (done) {
+  run(
+    'clean',
+    'copy',
+    done);
+});
 
 
 
+// gulp.task("copy", function () {
+//   return gulp.src([
+//     "source/fonts/**/*.{woff,woff2}",
+//     "source/img/*.{png,jpg}",
+//     "source/js/**",
+//     "source/**/*.html"
+//   ], {
+//     base: "source"
+//   })
+//     .pipe(gulp.dest("build"));
+// });
+//
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// gulp.task('mytask', function() {
-//   console.log('Привет, я таск!');
+// "use strict";
+//
+// var gulp = require("gulp");
+// var sass = require("gulp-sass");
+// var plumber = require("gulp-plumber");
+// var postcss = require("gulp-postcss");
+// var autoprefixer = require("autoprefixer");
+// var server = require("browser-sync").create();
+//
+// gulp.task("style", function() {
+//   gulp.src("source/sass/style.scss")
+//     .pipe(plumber())
+//     .pipe(sass())
+//     .pipe(postcss([
+//       autoprefixer()
+//     ]))
+//     .pipe(gulp.dest("source/css"))
+//     .pipe(server.stream());
+// });
+//
+// gulp.task("serve", ["style"], function() {
+//   server.init({
+//     server: "source/",
+//     notify: false,
+//     open: true,
+//     cors: true,
+//     ui: false
+//   });
+//
+//   gulp.watch("source/sass/**/*.{scss,sass}", ["style"]);
+//   gulp.watch("source/*.html").on("change", server.reload);
 // });
