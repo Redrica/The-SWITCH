0acb48ec2d559012c6175fd605d77decbacb4c9c
Redrica <de.spring@gmail.com>
2018-04-06 17:46:32 +0300

    Config gulp tasks for css and watching

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,30 @@
-"use strict";
+// "use strict";
 
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync');
 
+gulp.task('style', function () {
+  return gulp.src('source/sass/style.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('source/css'))
+    .pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('browser-sync', function () {
+  browserSync({
+    server: {
+      baseDir: 'source'
+    },
+    notify: false
+  });
+});
+
+gulp.task('watch', ['browser-sync', 'style'], function () {
+  gulp.watch('source/sass/style.scss', ['style']);
+  gulp.watch('source/*.html', browserSync.reload);
+  gulp.watch('source/js/*.js', browserSync.reload);
+});
 
 
 
